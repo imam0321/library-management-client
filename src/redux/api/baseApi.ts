650516225e835a8363d7aa3a5910637fa393b392
imam0321 <imam.hossain0321@gmail.com>
2025-07-05 23:47:17 +0600

    Update baseApi to use production API URL

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://ph-l2-b5-assignment-3.vercel.app/api" }),
   tagTypes: ["Books", "Borrow"],
   endpoints: () => ({}),
 });
